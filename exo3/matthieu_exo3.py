c00cbf73f3702fd859453e0b0e6d5f2b2ab966eb
Matthieu Fernandes <matthieu.fernandes@gmail.com>
2017-06-08 00:21:55 +0200

    exo 3 : unique element - solution

--- exo3/matthieu_exo3.py
+++ exo3/matthieu_exo3.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 
 def found_unique(l):
-    pass
+    i = l[0]
+    for j in l[1:]:
+        i = i ^ j
+    return i
 
 assert found_unique([1,1,2,2,3,4,4,5,5]) == 3
 assert found_unique([1,5,2,4,3,4,2,5,1]) == 3
