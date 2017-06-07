6c1166449cb8f1fe65c3be85151cab985843e4ef
Matthieu Fernandes <matthieu.fernandes@gmail.com>
2017-06-08 00:19:52 +0200

    exo 3 : unique element

---
+++ exo3/matthieu_exo3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def found_unique(l):
+    pass
+
+assert found_unique([1,1,2,2,3,4,4,5,5]) == 3
+assert found_unique([1,5,2,4,3,4,2,5,1]) == 3
+assert found_unique([1,1,2,2,3,3,4,4,5]) == 5
+assert found_unique([1,1,2,3,3,4,4,5,5]) == 2
